9de519483c2a4a677b6ee15112b6c12294c25332
shreeharib22 <shreeharib22@gmail.com>
2026-09-12 12:57:06 +0530

    Allow Vercel frontend in CORS

--- backend/main.py
+++ backend/main.py
@@ -35,6 +35,7 @@ app.add_middleware(
     "http://192.168.13.157:5174",
     "http://192.168.13.157:5175",
     "http://192.168.13.157:5176",
+    "https://sihgpt-ten.vercel.app",
     ],
     allow_credentials=True,
     allow_methods=["*"],
